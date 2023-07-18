db27b6fac5b20e8bd06e6b44171a04f5c06f186c
Pawel Walczewski <pwalczewski@gmail.com>
2023-07-18 20:19:52 +0200

    Add notes to people

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 # app.py
 
-from flask import Flask, render_template
+from flask import render_template # Remove: import Flask
+# import connexion
+import config
+from models import Person
+# app = Flask(__name__)
 
-app = Flask(__name__)
+# app = connexion.App(__name__, specification_dir="./")
+# app.add_api("swagger.yml")
+
+app = config.connexion_app
+app.add_api(config.basedir / "swagger.yml")
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    people = Person.query.all()
+    return render_template("home.html", people=people)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
